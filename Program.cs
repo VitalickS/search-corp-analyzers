1ebbcd9a6ac4368a0056f1e062f63286b708193e
Виталик Семеняко <jetframes@gmail.com>
2019-12-15 03:31:00 +0300

    initial analyzer-transform cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace search_corp_analyzers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Transforms/ImageProcessing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace search_corp_analyzers.Transforms
+{
+    public class ImageProcessing
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
